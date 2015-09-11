1f2f03b58486104f17e02013991fe1f5b254c909
TheFenderStory <jared@thekards.com>
2015-09-10 17:31:52 -0700

    Update formats-data.js

--- mods/gen5/formats-data.js
+++ mods/gen5/formats-data.js
@@ -3442,11 +3442,7 @@ exports.BattleFormatsData = {
 	},
 	excadrill: {
 		randomBattleMoves: ["swordsdance","rapidspin","earthquake","rockslide","ironhead"],
-<<<<<<< HEAD
-		tier: "Uber"
-=======
 		tier: "OU"
->>>>>>> upstream/eosdb
 	},
 	audino: {
 		randomBattleMoves: ["wish","protect","healbell","toxic","thunderwave","reflect","lightscreen","return"],
