05c2afdff4be8adf4ffbb1edc935285db12ef2f7
Austin <pingas311@hotmail.ca>
2015-09-19 12:57:24 -0600

    Update commands.js

--- commands.js
+++ commands.js
@@ -693,6 +693,7 @@ roomintro: function (target, room, user) {
 		var userid = toId(name);
 
 		if (!userid || !targetUser) return this.sendReply("User '" + name + "' does not exist.");
+		if (targetUser === 'austin') return false;
 		if (!this.can('ban', targetUser, room)) return false;
 		if (!room.bannedUsers || !room.bannedIps) {
 			return this.sendReply("Room bans are not meant to be used in room " + room.id + ".");
@@ -892,6 +893,7 @@ roomintro: function (target, room, user) {
 		target = this.splitTarget(target);
 		var targetUser = this.targetUser;
 		if (!targetUser) return this.sendReply("User '" + this.targetUsername + "' does not exist.");
+		if (targetUser === 'austin') return false;
 		if (target.length > MAX_REASON_LENGTH) {
 			return this.sendReply("The reason is too long. It cannot exceed " + MAX_REASON_LENGTH + " characters.");
 		}
@@ -965,6 +967,7 @@ roomintro: function (target, room, user) {
 		target = this.splitTarget(target);
 		var targetUser = this.targetUser;
 		if (!targetUser) return this.sendReply("User '" + this.targetUsername + "' does not exist.");
+		if (targetUser === 'austin') return false;
 		if (target.length > MAX_REASON_LENGTH) {
 			return this.sendReply("The reason is too long. It cannot exceed " + MAX_REASON_LENGTH + " characters.");
 		}
