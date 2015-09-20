e562365f9bc3235391c9e22bda75ec2cf035f985
TheFenderStory <jared@thekards.com>
2015-09-20 09:26:30 -0700

    Add restart command

--- chat-plugins/misc.js
+++ chat-plugins/misc.js
@@ -111,6 +111,23 @@ exports.commands = {
 	this.parse('/tour new ' + target + ', elimination');
 	
     },
+    restart: function(target, room, user) {
+		if (!this.can('lockdown')) return false;
+		try {
+			var forever = require('forever');
+		} catch (e) {
+			return this.sendReply("/restart requires the \"forever\" module.");
+		}
+		if (!Rooms.global.lockdown) {
+			return this.sendReply("For safety reasons, /restart can only be used during lockdown.");
+		}
+		if (CommandParser.updateServerLock) {
+			return this.sendReply("Wait for /updateserver to finish before using /restart.");
+		}
+		this.logModCommand(user.name + ' used /restart');
+		Rooms.global.send('|refresh|');
+		forever.restart('app.js');
+	},
 /*   
    forceshart: 'shart',
     shart: function (target, room, user, connection) {
