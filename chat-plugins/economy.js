2e2890fe90a3b6305d9394bbed7ea6411f8d5150
TheFenderStory <jared@thekards.com>
2015-09-20 18:34:12 -0700

    Fix sorting in richestusers

--- chat-plugins/economy.js
+++ chat-plugins/economy.js
@@ -325,22 +325,21 @@ exports.commands = {
 	richestusers: 'richestuser',
 	richestuser: function (target, room, user) {
 		if (!this.canBroadcast()) return;
-		var targetnum = target;
-        	if (!target || isNaN(target)) targetnum = 10;
-        	if (target > 25) targetnum = 25
 		var display = '<center><u><b>Richest Users</b></u></center><br><table border="1" cellspacing="0" cellpadding="5" width="100%"><tbody><tr><th>Rank</th><th>Username</th><th>Money</th></tr>';
-		var keys = Object.keys(Db('money'));
+		var keys = Object.keys(Db('money')).map(function(name) {
+			return {name: name, money: Db('money')[name]};
+		});
 		if (!keys.length) return this.sendReplyBox("Money ladder is empty.");
 		keys.sort(function (a, b) {
-			return b - a;
+			return b.money - a.money;
 		});
-		keys.slice(0, targetnum).forEach(function (user, index) {
-			display += "<tr><td>" + (index + 1) + "</td><td>" + user + "</td><td>" + Db('money')[user] + "</td></tr>";
+		keys.slice(0, 10).forEach(function (user, index) {
+			display += "<tr><td>" + (index + 1) + "</td><td>" + user.name + "</td><td>" + user.money + "</td></tr>";
 		});
 		display += "</tbody></table>";
 		this.sendReply("|raw|" + display);
 	},
-
+	
 	dicegame: 'startdice',
 	dicestart: 'startdice',
 	startdice: function (target, room, user) {
