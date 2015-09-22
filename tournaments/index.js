1e442400902e6e4a19d8320928ebbc53f1cb38c2
TheFenderStory <jared@thekards.com>
2015-09-21 23:00:57 -0700

    Update index.js

--- tournaments/index.js
+++ tournaments/index.js
@@ -791,10 +791,10 @@ Tournament = (function () {
 
 			Db.save();
 		}
-		if (this.room.isOfficial && tourSize >= 4) {
-            		var tourRarity = tourCard(tourSize, toId(winner));
-			this.room.addRaw("<b><font color='" + color + "'>" + Tools.escapeHTML(winner) + "</font> has also won a <font color=" + colors[tourRarity[0]] + ">" + tourRarity[0] + "</font> card: <button name='send' value='/card " + tourRarity[1] + "'>" + tourRarity[2] + "</button> from the tournament.");
-		}
+	//	if (this.room.isOfficial && tourSize >= 4) {
+        //  		var tourRarity = tourCard(tourSize, toId(winner));
+	//		this.room.addRaw("<b><font color='" + color + "'>" + Tools.escapeHTML(winner) + "</font> has also won a <font color=" + colors[tourRarity[0]] + ">" + tourRarity[0] + "</font> card: <button name='send' value='/card " + tourRarity[1] + "'>" + tourRarity[2] + "</button> from the tournament.");
+	//	}
 	};
 
 	return Tournament;
