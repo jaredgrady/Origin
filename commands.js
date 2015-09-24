5def2fab5322b2c507868bfeba47bde9d1ba0fde
Irraquated <hookerjackson22@gmail.com>
2015-09-25 01:50:22 +1000

    Yeah... I need sleep x_x

--- commands.js
+++ commands.js
@@ -224,7 +224,7 @@ var commands = exports.commands = {
 	ignorepm: 'ignorepms',
 	ignorepms: function (target, room, user) {
 		if (user.ignorePMs === (target || true)) return this.sendReply("You are already blocking private messages! To unblock, use /unblockpms");
-		if (user.can('lock') && !user.can('hotpatch')) return this.sendReply("You are not allowed to block private messages.");
+		if (user.can('lock') && !user.can('ban')) return this.sendReply("You are not allowed to block private messages.");
 		user.ignorePMs = true;
 		if (target in Config.groups) {
 			user.ignorePMs = target;
