840882d85326c5daf33f838c6ce103ceeba0253b
Irraquated <hookerjackson22@gmail.com>
2015-09-29 02:54:08 +1000

    Add the dev permission

--- users.js
+++ users.js
@@ -584,6 +584,7 @@ User = (function () {
 	User.prototype.can = function (permission, target, room) {
 		if (this.hasSysopAccess()) return true;
 		if (permission === 'vip' && Users.vips[this.userid] && !target) return true;
+		if (permission === 'dev' && ~developers.indexOf(this.userid)) return true;
 
 		var group = this.group;
 		var targetGroup = '';
