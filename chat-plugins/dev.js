caa75b282b158bb354b9eecc7befdd313b96ac46
Irraquated <hookerjackson22@gmail.com>
2015-09-29 23:45:25 +1000

    Update dev.js

--- chat-plugins/dev.js
+++ chat-plugins/dev.js
@@ -5,7 +5,7 @@ exports.commands = {
 		if (!~developers.indexOf(user.userid)) return this.errorReply("Access denied.");
 
 		for (var id in Rooms.rooms) {
-			if (id !== 'global') Rooms.rooms[id].addRaw('<div class="broadcast-blue"><small><u>' + 'Developer declare from: ' + user.userid + '</small></u><b>' + target + '</b></div>');
+			if (id !== 'global') Rooms.rooms[id].addRaw('<div class="broadcast-blue"><small><u>' + 'Developer declare from: ' + user.userid + '</small></u><br /><b>' + target + '</b></div>');
 		}
 		this.logModCommand(user.name + " dev declared " + target);
 	}  
