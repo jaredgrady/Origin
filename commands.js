1070df6a2b94d7df718e6d9224c6fc89d02deb51
thefenderstory <jgrady15@brophybroncos.org>
2015-10-29 11:13:39 -0700

    Fix roompromote for room leaders

--- commands.js
+++ commands.js
@@ -738,13 +738,18 @@ roomintro: function (target, room, user) {
 		if ((room.auth[userid] || Config.groupsranking[0]) === nextGroup) {
 			return this.errorReply("User '" + name + "' is already a " + groupName + " in this room.");
 		}
-		if (!user.can('makeroom')) {
+		if (!user.can('makeroom') && !user.can('declare', null, room)) {
 			if (currentGroup !== ' ' && !user.can('room' + (Config.groups[currentGroup] ? Config.groups[currentGroup].id : 'voice'), null, room)) {
 				return this.errorReply("/" + cmd + " - Access denied for promoting/demoting from " + (Config.groups[currentGroup] ? Config.groups[currentGroup].name : "an undefined group") + ".");
 			}
 			if (nextGroup !== ' ' && !user.can('room' + Config.groups[nextGroup].id, null, room)) {
 				return this.errorReply("/" + cmd + " - Access denied for promoting/demoting to " + Config.groups[nextGroup].name + ".");
 			}
+		}
+		if (room.auth[user.userid] === '&' && room.founder !== user.userid) {
+			if (target === '&' || target === '#') {
+			return this.errorReply("/" + cmd + " - Access denied for promoting/demoting to " + Config.groups[nextGroup].name + ".");
+		    }
 		}
 
 		if (nextGroup === ' ') {
