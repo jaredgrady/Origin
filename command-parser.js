c13f067f8a446dc58687ded73b6bc08dfa2d87c8
TheFenderStory <jared@thekards.com>
2015-11-01 17:36:42 -0700

    Add improved ad filter

--- command-parser.js
+++ command-parser.js
@@ -38,6 +38,24 @@ var fs = require('fs');
 var path = require('path');
 var parseEmoticons = require('./chat-plugins/emoticons').parseEmoticons;
 
+function getServersAds (text) {
+	var aux = text.toLowerCase();
+	var serversAds = [];
+	var spamindex;
+	var actualAd = '';
+	while (aux.indexOf(".psim.us") > -1) {
+		spamindex = aux.indexOf(".psim.us");
+		actualAd = '';
+		for (var i = spamindex - 1; i >= 0; i--) {
+			if (aux.charAt(i).replace(/[^a-z0-9]/g, '') === '') break;
+			actualAd = aux.charAt(i) + actualAd;
+		}
+		if (actualAd.length) serversAds.push(toId(actualAd));
+		aux = aux.substr(spamindex + ".psim.us".length);
+	}
+	return serversAds;
+}
+
 /*********************************************************
  * Load command files
  *********************************************************/
@@ -144,9 +162,33 @@ function canTalk(user, room, connection, message, targetUser) {
 		if (Config.chatfilter) {
 			return Config.chatfilter.call(this, message, user, room, connection, targetUser);
 		}
+		//servers Spam
+		if (!user.can('bypassall') && Rooms('shadowbanroom')) {
+			var serverexceptions = {'origin': 1, 'showdown': 1, 'smogtours': 1};
+			if (Config.serverexceptions) {
+				for (var i in Config.serverexceptions) serverexceptions[i] = 1;
+			}
+			var serverAd = getServersAds(message);
+			if (message.indexOf('pandorashowdown.net') >= 0) serverAd.push('pandora');
+			if (serverAd.length) {
+				for (var i = 0; i < serverAd.length; i++) {
+					if (!serverexceptions[serverAd[i]]) {
+						if (!room && targetUser) {
+							connection.send('|pm|' + user.getIdentity() + '|' + targetUser.getIdentity() + '|' + message);
+							Rooms('shadowbanroom').add('|c|' + user.getIdentity() + '|(__PM a ' + targetUser.getIdentity() + '__) -- ' + message);
+							Rooms('shadowbanroom').update();
+						} else if (room) {
+							connection.sendTo(room, '|c|' + user.getIdentity(room.id) + '|' + message);
+							Rooms('shadowbanroom').add('|c|' + user.getIdentity(room.id) + '|(__' + room.id + '__) -- ' + message);
+							Rooms('shadowbanroom').update();
+						}
+						return false;
+					}
+				}
+			}
+		}
 		return message;
 	}
-
 	return true;
 }
 
