d969c0998a42d151c117e004b31230c2e7fbb756
TheFenderStory <jared@thekards.com>
2015-11-09 22:14:40 -0700

    Fix spacing in room item

--- chat-plugins/economy.js
+++ chat-plugins/economy.js
@@ -11,7 +11,7 @@ var shop = [
     ['Avatar', 'Buys an custom avatar to be applied to your name. (You supply. Images larger than 80x80 may not show correctly. Gifs are broken atm.)', 25],
     ['Trainer', 'Buys a trainer card which shows information through a command. (You supply, can be refused).', 40],
     ['League Shop', 'Purchases a League Shop for use in your league room, room must be a league room.', 70],
-    ['Room', 'Buys a chatroom for you to own.  (Can be deleted if it goes inactive for too long.Within reason, can be refused)', 90],
+    ['Room', 'Buys a chatroom for you to own. (Can be deleted if it goes inactive for too long. Within reason, can be refused)', 90],
     ['Custom Emote', 'Buys a custom emote to be displays when the command is entered. (command must start with feels and size must be 50x50)', 100],
     ['Userlist Icon', 'Buys a 32x32 userlist icon supplied by you that will show in 3 rooms. (We will not change the rooms even if a fix is purchased. Will take time to appear. PM Master Float, Jack (~\'v\')~ or CreaturePhil once you bought one.)', 350]
 ];
