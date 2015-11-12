8482ff372c179f567d8eed4a444f263d05666e49
Austin Couturier <pingas311@hotmail.ca>
2015-11-12 12:14:22 -0700

    Make CE tournaments readable

--- data/aliases.js
+++ data/aliases.js
@@ -32,6 +32,7 @@ exports.BattleAliases = {
 	"inverse": "Inverse Battle",
 	"aaa": "Almost Any Ability",
 	"monorandom": "Monotype Random Battle",
+	"ce": "Catch and Evolve",
 
 	// mega evos
 	"maero": "Aerodactyl-Mega",
