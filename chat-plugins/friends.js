1cc425201adb088c0052038ff083cdde348fa2e1
TheFenderStory <jared@thekards.com>
2015-11-12 10:39:46 -0700

    Fix friend list display

--- chat-plugins/friends.js
+++ chat-plugins/friends.js
@@ -1,20 +1,26 @@
 //Friends List 
 function getFriendList(user) {
 	var list = Db('friends')[user.userid];
-	var display = '<table style="width: 100%; border: 1px solid #803C6F; border-top-right-radius: 4px; border-top-left-radius: 4px; background: rgba(205, 159, 196, 0.7);"><tr>';
+	var display = '<table style="width: 100%; border: 1px solid #803C6F; border-top-right-radius: 4px; border-top-left-radius: 4px; background: rgba(205, 159, 196, 0.7);"><tr><td>';
 	var start = 0;
 	if (!list) return "You have no friends. :(";
 	if (list.length === 0) return "You have no friends :(";
 	while (start < list.length) {
 		if (!Users.get(list[start])) {
 			display += '<td style="background: rgba(255, 255, 255, 0.5); border: 1px solid #803C6F; padding: 5px; border-radius: 4px; text-align: center;"><button name="parseCommand" value="/user ' + list[start] + '" style="border: 1px solid #803C6F; background: #c4c4c4; color: #502243; text-shadow: 0px 0px 2px #FCE8F1; padding: 5px; border-radius: 4px;">' + list[start] + '</button></td>'; 
+			if (list.length % 5 ===0) {
+				display += '</td><td>';
+			}
 			start++; 
 		} else {
 			display += '<td style="background: rgba(255, 255, 255, 0.5); border: 1px solid #803C6F; padding: 5px; border-radius: 4px; text-align: center;"><button name="parseCommand" value="/user ' + list[start] + '" style="border: 1px solid #803C6F; background: #CD9FC4; color: #502243; text-shadow: 0px 0px 2px #FCE8F1; padding: 5px; border-radius: 4px;">' + list[start] + '</button></td>'; 
+			if (list.length % 5 ===0) {
+				display += '</td><td>';
+			}
 			start++; 
 		}
 	}
-	display += '</tr></table>';
+	display += '</td></tr></table>';
 	return display;
 };
 
