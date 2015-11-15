bc22f7d7647d359905e8483194b3a5decfeb2dcc
Austin Couturier <pingas311@hotmail.ca>
2015-11-15 00:22:17 -0700

    make devdeclare look better

--- chat-plugins/dev.js
+++ chat-plugins/dev.js
@@ -2,10 +2,10 @@ exports.commands = {
     devdeclare: 'ddeclare',
     ddeclare: function (target, room, user) {
 		if (!target) return this.parse('/help globaldeclare');
-		if (!~developers.indexOf(user.userid)) return this.errorReply("Access denied.");
+		if (!~developers.indexOf(user.userid)) return this.errorReply("/devdeclare - Access denied.");
 
 		for (var id in Rooms.rooms) {
-			if (id !== 'global') Rooms.rooms[id].addRaw('<div class="broadcast-blue"><small><u>' + 'Developer declare from: ' + user.userid + '</small></u><br /><b>' + target + '</b></div>');
+			if (id !== 'global') Rooms.rooms[id].addRaw('<div class="broadcast-black"><large><i>' + 'Developer declare from: ' + user.userid + '</i></large><br /><b>' + target + '</b></div>');
 		}
 		this.logModCommand(user.name + " dev declared " + target);
 	}  
