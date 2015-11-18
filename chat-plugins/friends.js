8da31940c5c5e48e7bea3e5df1a1b2e45b46e96d
thefenderstory <jgrady15@brophybroncos.org>
2015-11-18 12:15:52 -0700

    Update friends

--- chat-plugins/friends.js
+++ chat-plugins/friends.js
@@ -1,26 +1,24 @@
-//Friends List 
+// friends list
 function getFriendList(user) {
 	var list = Db('friends')[user.userid];
-	var display = '<table style="width: 100%; border: 1px solid #803C6F; border-top-right-radius: 4px; border-top-left-radius: 4px; background: rgba(205, 159, 196, 0.7);"><tr><td>';
+	var display = '<table style="width: 100%; border: 1px solid #803C6F; border-top-right-radius: 4px; border-top-left-radius: 4px; background: rgba(205, 159, 196, 0.7);"><tr>';
 	var start = 0;
 	if (!list) return "You have no friends. :(";
 	if (list.length === 0) return "You have no friends :(";
 	while (start < list.length) {
+                if ((start + 1) % 5) {
+                     display += '</tr><tr><td style="background: rgba(255, 255, 255, 0.5); border: 1px solid #803C6F; padding: 5px; border-radius: 4px; text-align: center;"><button name="parseCommand" value="/user ' + list[start] + '" style="border: 1px solid #803C6F; background: #c4c4c4; color: #502243; text-shadow: 0px 0px 2px #FCE8F1; padding: 5px; border-radius: 4px;">' + list[start] + '</button></td>'; 
+		     start++;
+                }
 		if (!Users.get(list[start])) {
 			display += '<td style="background: rgba(255, 255, 255, 0.5); border: 1px solid #803C6F; padding: 5px; border-radius: 4px; text-align: center;"><button name="parseCommand" value="/user ' + list[start] + '" style="border: 1px solid #803C6F; background: #c4c4c4; color: #502243; text-shadow: 0px 0px 2px #FCE8F1; padding: 5px; border-radius: 4px;">' + list[start] + '</button></td>'; 
-			if (list[start] % 5 === 0) {
-				display += '</tr><tr>';
-			}
 			start++; 
 		} else {
 			display += '<td style="background: rgba(255, 255, 255, 0.5); border: 1px solid #803C6F; padding: 5px; border-radius: 4px; text-align: center;"><button name="parseCommand" value="/user ' + list[start] + '" style="border: 1px solid #803C6F; background: #CD9FC4; color: #502243; text-shadow: 0px 0px 2px #FCE8F1; padding: 5px; border-radius: 4px;">' + list[start] + '</button></td>'; 
-			if (list[start] % 5 === 0) {
-				display += '</tr><tr>';
-			}
 			start++; 
 		}
 	}
-	display += '</td></tr></table>';
+	display += '</tr></table>';
 	return display;
 };
 
@@ -62,7 +60,7 @@ exports.commands = {
 		var targetUser = toId(target);	
 		
 		if (Db('friends')[userid].indexOf(targetUser) > -1) {
-			Db('friends')[userid].splice(targetUser, 1);
+			Db('friends')[userid].splice(1, targetUser);
 			Db.save();
 			this.sendReply(targetUser + " has been removed from your friends list.");
 		} else {
