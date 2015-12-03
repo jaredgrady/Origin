a0ab78ab0ad9ab18f17143c9901e6c017cd2653e
Austin Couturier <pingas311@hotmail.ca>
2015-12-03 01:06:06 -0700

    Fix shop declares

--- chat-plugins/economy.js
+++ chat-plugins/economy.js
@@ -266,8 +266,8 @@ exports.commands = {
 
 	store: 'shop',
 	shop: function (target, room, user) {
-		if (room.id === 'lobby' || room.battle || room.id === 'japanslair') return this.sendReply("This command is too spammy for lobby/battles.");
 		if (!this.canBroadcast()) return;
+		if (room.id === 'lobby' || room.battle) return this.sendReply("This command is too spammy for lobby/battles.");
 		return this.sendReply("|raw|" + shopDisplay);
 	},
 	shophelp: ["/shop - Display items you can buy with money."],
