ce2c3d38ed69d3c6e643db29d9784c6364eac932
MasterFloat <themasterfloat@gmail.com>
2015-12-12 07:35:04 +0100

    Better block
    This will make people unable to do !shop in lobby or battles, though unlike the old block people will be able to do /shop in lobby or battles if they want to.

--- chat-plugins/economy.js
+++ chat-plugins/economy.js
@@ -294,8 +294,9 @@ exports.commands = {
 
 	store: 'shop',
 	shop: function (target, room, user) {
-		if (!this.canBroadcast()) return;
-		if (room.id === 'lobby' && !user.can('bypassall') || room.battle && !user.can('bypassall')) return this.sendReply("This command is too spammy for lobby/battles.");
+		if (room.id !== 'lobby' && !room.battle) {
+			if (!this.canBroadcast()) return;
+		}
 		return this.sendReply("|raw|" + shopDisplay);
 	},
 	shophelp: ["/shop - Display items you can buy with money."],
