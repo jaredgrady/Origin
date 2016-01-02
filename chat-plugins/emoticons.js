c5a86a8d6563d099f05aabee1445513d75f064ee
sparkychild <pikachildyellow@gmail.com>
2016-01-02 11:43:35 -0500

    add the ones Neo Soul gave me
    have verified that gifs work

--- chat-plugins/emoticons.js
+++ chat-plugins/emoticons.js
@@ -92,7 +92,10 @@ var emotes = {
 	'yayface': 'http://i.imgur.com/anY1jf8.png',
 	'yesface': 'http://i.imgur.com/k9YCF6K.png',
 	'youdontsay': 'http://r32.imgfast.net/users/3215/23/26/64/smiles/280467785.jpg',
-	'feelslelouch': 'http://i.imgur.com/qZrV75o.png'
+	'feelslelouch': 'http://i.imgur.com/qZrV75o.png',
+	'bsmiss': 'http://i.imgur.com/C235Ljo.gif',
+	'bshax': 'http://i.imgur.com/scBu82l.gif',
+	'bscrit': 'http://i.imgur.com/xVqLpF0.gif'
 };
 
 var emotesKeys = Object.keys(emotes);
