58a7c30ec88fb61d2b9d44e67b749baa8bfd144e
Jared Grady <jared@thekards.com>
2016-01-04 07:41:37 -0700

    Fix crash in memusage

--- commands.js
+++ commands.js
@@ -2107,8 +2107,8 @@ let commands = exports.commands = {
 	crashfixedhelp: ["/crashfixed - Ends the active lockdown caused by a crash without the need of a restart. Requires: ~"],
 
 	'memusage': 'memoryusage',
-	memoryusage: function (target, user) {
-		if (!~developers.indexOf(user.userid)) return false;
+	memoryusage: function (target) {
+		if (!this.can('hotpatch')) return false;
 		let memUsage = process.memoryUsage();
 		let results = [memUsage.rss, memUsage.heapUsed, memUsage.heapTotal];
 		let units = ["B", "KiB", "MiB", "GiB", "TiB"];
