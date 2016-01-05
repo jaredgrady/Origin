651a468bc98e6c661b01add3c45b202273ca93a5
Jared Grady <jared@thekards.com>
2016-01-05 15:13:22 -0700

    Fix richesusers and economy stats

--- chat-plugins/economy.js
+++ chat-plugins/economy.js
@@ -371,26 +371,16 @@ exports.commands = {
 	richladder: 'richestuser',
 	richestusers: 'richestuser',
 	richestuser: function (target, room, user) {
-		let nTarget = Number(target);
-		if (!target || nTarget <= 10) { // The command can only be broadcasted if there's no set number or if the number is smaller or equal to 10
-			if (!this.canBroadcast()) return;
-		}
-		if (nTarget > 100) return this.errorReply("Richestusers is limited to the top 100 richest users.");
+		if (!this.canBroadcast()) return;
 		let display = '<center><u><b>Richest Users</b></u></center><br><table border="1" cellspacing="0" cellpadding="5" width="100%"><tbody><tr><th>Rank</th><th>Username</th><th>Money</th></tr>';
 		let keys = Object.keys(Db('money').object()).map(function (name) {
 			return {name: name, money: Db('money').get(name)};
 		});
 		if (!keys.length) return this.sendReplyBox("Money ladder is empty.");
 		keys.sort(function (a, b) { return b.money > a.money; });
-		if (!target || !isNumber(target)) {
-			keys.slice(0, 10).forEach(function (user, index) {
-				display += "<tr><td>" + (index + 1) + "</td><td>" + user.name + "</td><td>" + user.money + "</td></tr>";
-			});
-		} else {
-			keys.slice(0, nTarget).forEach(function (user, index) { // slices between 0 and the specified amount
-				display += "<tr><td>" + (index + 1) + "</td><td>" + user.name + "</td><td>" + user.money + "</td></tr>";
-			});
-		}
+		keys.slice(0, 10).forEach(function (user, index) {
+			display += "<tr><td>" + (index + 1) + "</td><td>" + user.name + "</td><td>" + user.money + "</td></tr>";
+		});
 		display += "</tbody></table>";
 		this.sendReply("|raw|" + display);
 	},
