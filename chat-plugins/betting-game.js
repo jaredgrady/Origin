5ba10e6175015b506a222db7d7cd78dfe93ec138
MasterFloat <themasterfloat@gmail.com>
2016-01-11 08:44:10 +0100

    Tweaks

--- chat-plugins/betting-game.js
+++ chat-plugins/betting-game.js
@@ -1,5 +1,7 @@
 'use strict'
 
+let color = require('../config/color');
+
 let diceOne = function(dice) {
 	dice = Math.floor(Math.random() * 6) + 1;
 	return dice;
@@ -8,11 +10,6 @@ let diceOne = function(dice) {
 let diceTwo = function(dice) {
 	dice = Math.floor(Math.random() * 6) + 1;
 	return dice;
-}
-
-let rng = function(n) {
-	n = Math.floor(Math.random() * 100);
-	return n
 }
 
 function isEven(n) {
@@ -30,42 +27,40 @@ exports.commands = {
 		let secondDice = diceTwo();
 		let totalDice = firstDice + secondDice;
 
-		let chances = rng();
-
 		let choice = target.toUpperCase();
 
 		let amount = Db('money').get(user.userid, 0);
 
-		if (amount < 1) return this.errorReply("You don't have enough bucks for the bet.");
+		if (amount < 2) return this.errorReply("You don't have enough bucks for the bet.");
 
 		if(!target) return this.parse('/help bet');
 
 		switch(choice) {
 			case 'ODD':
-				Db('money').set(user.userid, amount - 1).get(user.userid);
+				Db('money').set(user.userid, amount - 2).get(user.userid);
 				if (isOdd(totalDice)) {
-					this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><center><h2 style="color: #444">' + user.name + '\'s both dices rolled a<br />total of <font style="color: #f00 ; text-decoration: underline">'+ totalDice + '</font>.</h2></center><br /><center><h2 style="color: #444">You Win!!</h2></center></div></div></div>');
+					this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><center><h2 style="color: #444"><font color="' + color(toId(this.user.name)) + '">' + user.name + '</font>\'s both dices rolled a<br />total of <font style="color: #f00 ; text-decoration: underline">'+ totalDice + '</font>.</h2></center><br /><center><h2 style="color: #444">You Win!!</h2></center></div></div></div>');
 					Db('money').set(user.userid, amount + 2).get(user.userid);
 				}else{
-					this.sendReply(":s...better luck next time...");
+					this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><center><h2 style="color: #444"><font color="' + color(toId(this.user.name)) + '">' + user.name + '</font>\'s both dices rolled a<br />total of <font style="color: #f00 ; text-decoration: underline">'+ totalDice + '</font>.</h2></center><br /><center><h2 style="color: #444">You lose... better luck next time!</h2></center></div></div></div>');
 				}
 			break;
 			case 'EVEN':
-				Db('money').set(user.userid, amount - 1).get(user.userid);
+				Db('money').set(user.userid, amount - 2).get(user.userid);
 				if (isEven(totalDice)) {
-					this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><center><h2 style="color: #444">' + user.name + '\'s both dices rolled a<br />total of <font style="color: #f00 ; text-decoration: underline">'+ totalDice + '</font>.</h2></center><br /><center><h2 style="color: #444">You Win!!</h2></center></div></div></div>');
+					this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><center><h2 style="color: #444"><font color="' + color(toId(this.user.name)) + '">' + user.name + '</font>\'s both dices rolled a<br />total of <font style="color: #f00 ; text-decoration: underline">'+ totalDice + '</font>.</h2></center><br /><center><h2 style="color: #444">You Win!!</h2></center></div></div></div>');
 					Db('money').set(user.userid, amount + 2).get(user.userid);
 				}else{
-					this.sendReply(":s...better luck next time...");
+					this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><center><h2 style="color: #444"><font color="' + color(toId(this.user.name)) + '">' + user.name + '</font>\'s both dices rolled a<br />total of <font style="color: #f00 ; text-decoration: underline">'+ totalDice + '</font>.</h2></center><br /><center><h2 style="color: #444">You lose... better luck next time!</h2></center></div></div></div>');
 				}
 			break;
 			case '7': case 'SEVEN':
-				Db('money').set(user.userid, amount - 1).get(user.userid);
-				if (totalDice === 7 && chances > 70) {
-					this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><center><h2 style="color: #444">' + user.name + '\'s both dices rolled a<br />total of <font style="color: #f00 ; text-decoration: underline">'+ totalDice + '</font>.</h2></center><br /><center><h2 style="color: #444">You Win!!</h2></center></div></div></div>');
+				Db('money').set(user.userid, amount - 2).get(user.userid);
+				if (totalDice === 7) {
+					this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><center><h2 style="color: #444"><font color="' + color(toId(this.user.name)) + '">' + user.name + '</font>\'s both dices rolled a<br />total of <font style="color: #f00 ; text-decoration: underline">'+ totalDice + '</font>.</h2></center><br /><center><h2 style="color: #444">You Win!!</h2></center></div></div></div>');
 					Db('money').set(user.userid, amount + 6).get(user.userid);
 				}else{
-					this.sendReply(":s...better luck next time...");
+					this.sendReply('|raw|<div class="infobox" style="background: rgba(190 , 190 , 190 , 0.4) ; border-radius: 2px"><div style="background: url(&quot;http://i.imgur.com/otpca0K.png?1&quot;) left center no-repeat"><div style="background: url(&quot;http://i.imgur.com/rrq3gEp.png&quot;) right center no-repeat"><center><h2 style="color: #444"><font color="' + color(toId(this.user.name)) + '">' + user.name + '</font>\'s both dices rolled a<br />total of <font style="color: #f00 ; text-decoration: underline">'+ totalDice + '</font>.</h2></center><br /><center><h2 style="color: #444">You lose... better luck next time!</h2></center></div></div></div>');
 				}
 			break;
 			default:
