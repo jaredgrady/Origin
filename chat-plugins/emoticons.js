5a839b70ccd5c5672a3ac64c6de16d976500c28a
MasterFloat <themasterfloat@gmail.com>
2016-01-25 11:15:09 +0100

    feelsfloat!!!

--- chat-plugins/emoticons.js
+++ chat-plugins/emoticons.js
@@ -100,7 +100,8 @@ let emotes = {
 	'yayface': 'http://i.imgur.com/anY1jf8.png',
 	'yesface': 'http://i.imgur.com/k9YCF6K.png',
 	'youdontsay': 'http://r32.imgfast.net/users/3215/23/26/64/smiles/280467785.jpg',
-	'gudone': 'http://i.imgur.com/USkp1b9.png'
+	'gudone': 'http://i.imgur.com/USkp1b9.png',
+	'feelsfloat': 'http://i.imgur.com/XKP1Kpf.gif'
 };
 
 let emotesKeys = Object.keys(emotes);
