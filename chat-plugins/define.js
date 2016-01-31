cb0b6f63e37e3ec9f5abad8fda2b7cee652524b7
Phil <birkal@outlook.com>
2016-01-30 21:43:43 -0800

    FP Define

--- chat-plugins/define.js
+++ chat-plugins/define.js
@@ -2,65 +2,61 @@
 
 const request = require('request');
 
-const urbandefine = require('../urbandefine');
+const def = require('../define');
+const define = def.define;
+const urbandefine = def.urbandefine;
 
 exports.commands = {
 	def: 'define',
 	define: function (target, room, user) {
+		if (!this.canBroadcast()) return;
 		if (!target) return this.parse('/help define');
-		target = toId(target);
-
-		if (target > 50) return this.errorReply("Word can not be longer than 50 characters.");
-		if (room.id !== 'lobby' || !room.battle) {
-			if (!this.canBroadcast()) return;
-		}
+		if (target.length > 50) return this.errorReply("Phrase cannot be longer than 50 characters.");
 		if (!this.canTalk()) return this.errorReply("You cannot do this while unable to speak.");
 
-		let options = {
-			url: 'http://api.wordnik.com:80/v4/word.json/' + target + '/definitions?limit=3&sourceDictionaries=all' +
-			'&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5',
-		};
+		const targetId = toId(target);
+		const self = this;
 
-		let self = this;
-
-		function callback(error, response, body) {
-			if (!error && response.statusCode === 200) {
-				let page = JSON.parse(body);
-				let output = "<font color=#24678d><b>Definitions for " + target + ":</b></font><br />";
-				if (!page[0]) {
-					self.sendReplyBox("No results for <b>\"" + target + "\"</b>.");
-					return room.update();
-				} else {
-					let count = 1;
-					for (let u in page) {
-						if (count > 3) break;
-						output += "(<b>" + count + "</b>) " + Tools.escapeHTML(page[u]['text']) + "<br />";
-						count++;
-					}
-					self.sendReplyBox(output);
-					return room.update();
-				}
-			}
+		if (Db('defcache').get(targetId)) {
+			return this.sendReplyBox("<b>" + Tools.escapeHTML(target) + ":</b> " + Db('defcache').get(targetId));
 		}
-		request(options, callback);
+
+		define(targetId).fork(
+			function error(err) {
+				console.error(err);
+			},
+			function success(maybe) {
+				if (!maybe.value) {
+					self.sendReplyBox("No results for <b>\"" + Tools.escapeHTML(target) + "\"</b>.");
+					room.update();
+				}
+				maybe.map(function (definition) {
+					const sanitizeDef = definition.slice(0, 400).replace(/\r\n/g, '<br />').replace(/\n/g, ' ');
+					const trimDef = definition.length > 400 ? sanitizeDef + '...' : sanitizeDef;
+					const output = "<b>" + Tools.escapeHTML(target) + ":</b> " + trimDef;
+					Db('defcache').set(targetId, trimDef);
+					self.sendReplyBox(output);
+					room.update();
+				});
+			}
+		);
 	},
 	definehelp: ["/define [word] - Shows the definition of a word."],
 
 	u: 'ud',
 	urbandefine: 'ud',
-	ud: function (target, room, user, connection, cmd) {
+	ud: function (target, room, user) {
 		if (!this.canBroadcast()) return;
 		if (!target) return this.parse('/help ud');
 		if (target.length > 50) return this.errorReply("Phrase cannot be longer than 50 characters.");
 		if (!this.canTalk()) return this.errorReply("You cannot do this while unable to speak.");
 
 		const targetId = toId(target);
+		const self = this;
 
 		if (Db('udcache').get(targetId)) {
 			return this.sendReplyBox("<b>" + Tools.escapeHTML(target) + ":</b> " + Db('udcache').get(targetId));
 		}
-
-		const self = this;
 
 		urbandefine(targetId).fork(
 			function error(err) {
