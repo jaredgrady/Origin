361bf78b3edaf2a87b7da4e195df50029f046fbc
Jared Grady <jared@thekards.com>
2016-02-05 01:31:49 -0700

    Fix travis in rankladder

--- rank-ladder.js
+++ rank-ladder.js
@@ -15,7 +15,7 @@ function rankLadder(title, type, array, prop) {
 	const first = 'class="first default-td" style="box-shadow: -1px -1px 2px rgba(0, 0, 0, 0.3) inset, 1px 1px 1px rgba(255, 255, 255, 0.7) inset;"';
 	const second = 'class="second default-td" style="box-shadow: -1px -1px 2px rgba(0, 0, 0, 0.3) inset, 1px 1px 1px rgba(255, 255, 255, 0.7) inset;"';
 	const third = 'class="third default-td" style="box-shadow: -1px -1px 2px rgba(0, 0, 0, 0.3) inset, 1px 1px 1px rgba(255, 255, 255, 0.7) inset;"';
-	let midColumn; 
+	let midColumn;
 	const length = array.length;
 
 	let tableRows = '';
@@ -24,12 +24,10 @@ function rankLadder(title, type, array, prop) {
 		if (i === 0) {
 			midColumn = '</td><td ' + first + '>';
 			tableRows += '<tr><td ' + first + '>' + (i + 1) + midColumn + array[i].name + midColumn + array[i][prop] + '</td></tr>';
-		}
-		else if (i === 1) {
+		} else if (i === 1) {
 			midColumn = '</td><td ' + second + '>';
 			tableRows += '<tr><td ' + second + '>' + (i + 1) + midColumn + array[i].name + midColumn + array[i][prop] + '</td></tr>';
-		}
-		else if (i === 2) {
+		} else if (i === 2) {
 			midColumn = '</td><td ' + third + '>';
 			tableRows += '<tr><td ' + third + '>' + (i + 1) + midColumn + array[i].name + midColumn + array[i][prop] + '</td></tr>';
 		} else {
@@ -37,7 +35,6 @@ function rankLadder(title, type, array, prop) {
 			tableRows += '<tr><td ' + tdStyle + '>' + (i + 1) + midColumn + array[i].name + midColumn + array[i][prop] + '</td></tr>';
 		}	
 	}
-
 	return ladderTitle + tableTop + tableRows + tableBottom;
 }
 
