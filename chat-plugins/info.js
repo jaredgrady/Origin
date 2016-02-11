dae5ed0f9cc8b858220af1072962edfc7aeec98a
Austin Couturier <pingas311@hotmail.ca>
2016-02-11 01:10:04 -0700

    Fix travis

--- chat-plugins/info.js
+++ chat-plugins/info.js
@@ -28,7 +28,7 @@ exports.commands = {
 			return this.errorReply("User " + this.targetUsername + " not found.");
 		}
 		if (!user.isSysop && targetUser.isSysop) {
-			return this.errorReply("You are not allowed to have this information.")
+			return this.errorReply("You are not allowed to have this information.");
 		}
 		let showAll = (cmd === 'ip' || cmd === 'whoare' || cmd === 'alt' || cmd === 'alts');
 		if (showAll && !user.confirmed && targetUser !== user) {
