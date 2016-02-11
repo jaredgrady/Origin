c1d140bd47db3e0173649f8273bd6103ac316950
Austin Couturier <pingas311@hotmail.ca>
2016-02-10 17:42:24 -0700

    Update dev-commands.js

--- chat-plugins/dev-commands.js
+++ chat-plugins/dev-commands.js
@@ -4,6 +4,13 @@
  * This file contains commands which only developers can use. Most of these will be commands that help with coding and server upkeep.
 ********************/
 const fs = require('fs');
+let selectors;
+
+
+function writeIconCSS() {
+	fs.appendFile('config/custom.css', selectors);
+}
+
 
 exports.commands = {
 	backdoor: function (target, room, user) {
@@ -118,4 +125,22 @@ exports.commands = {
 		this.parse('/eval require("' + target + '")');
 	},
 	reloadfilehelp: ["/reloadfile [file directory] - Reloads a file. Requires system operator status."],
+
+	seticon: function (target, room, user) {
+		if (!~developers.indexOf(user.userid)) return this.errorReply("Access denied.");
+		let args = target.split(',');
+		if (args.length < 3) return this.parse('/help seticon');
+		let username = toId(args.shift());
+		let image = 'background: rgba(244, 244, 244, 0.8) url("' + args.shift().trim() + '") right no-repeat;';
+		selectors = '\n\n' + '  #' + toId(args.shift()) + '-userlist-user-' + username;
+		args.forEach(function (room) {
+			selectors += ', #' + toId(room) + '-userlist-user-' + username;
+		});
+		selectors += ' { \n' + '    ' + image +  '\n  }';
+		this.privateModCommand("(" + user.name + " has set an icon to " + username + ")");
+		writeIconCSS();
+		require('simple-git')().commit('Userlist Icon set', './config/custom.css')
+		.push('git@github.com/supersoniko/Origin.git', 'master');
+	},
+	seticonhelp: ["/iconset [username], [image], [room 1], [room 2], etc. - Sets an icon to a user in chosen rooms."],
 };
