5a4863b6947bf125eca0c3c245e767e33dfe2d95
AuraStormLucario <yolo2infinity@icloud.com>
2016-02-17 11:48:59 -0500

    travis

--- chat-plugins/user-commands.js
+++ chat-plugins/user-commands.js
@@ -168,7 +168,7 @@ exports.commands = {
 		if (user.can('lock', null, room)) {
 			this.add("|raw|-- <font color='" + color(user.userid) + "'><strong>" + Tools.escapeHTML(newName) + "</strong></font> is no longer " + status.toLowerCase() + ".");
 			this.parse("/show");
-		}						
+		}
 	},
 	backhelp: ['/back - Sets a users away status back to normal.'],
 
