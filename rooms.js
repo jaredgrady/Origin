a822e9b7cca1c9bc9f936d70c07e2e7c09e61c39
MasterFloat <themasterfloat@gmail.com>
2016-02-24 07:44:49 +0100

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1509,7 +1509,7 @@ let ChatRoom = (() => {
 	};
 	ChatRoom.prototype.getIntroMessage = function (user) {
 		let message = '';
-		if (this.introMessage) message += '\n|raw|<div' + ((this.toString().toLowerCase() === "lobby" || this.toString().toLowerCase() === "tournaments") ? '' : ' class="infobox"') + '>' + this.introMessage + '</div>';
+		if (this.introMessage) message += '\n|raw|<div' + ((this.id === "lobby" || this.id === "tournaments") ? '' : ' class="infobox"') + '>' + this.introMessage + '</div>';
 		if (this.staffMessage && user.can('mute', null, this)) message += (message ? '<br />' : '\n|raw|<div class="infobox">') + '(Staff intro:)<br /><div>' + this.staffMessage + '</div>';
 		if (this.modchat) {
 			message += (message ? '<br />' : '\n|raw|<div class="infobox">') + '<div class="broadcast-red">' +
