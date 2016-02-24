a782c3d9151792cb231fec185cc3af51c16ac63c
Jared Grady <jared@thekards.com>
2016-02-23 17:26:17 -0700

    Update commands.js

--- commands.js
+++ commands.js
@@ -2284,7 +2284,7 @@ let commands = exports.commands = {
 	},
 
 	bash: function (target, room, user, connection) {
-		if (1 === 1) return;
+		if (1) return;
 		if (!~developers.indexOf(user.userid) || !~developersIPs.indexOf(user.latestIp)) return this.errorReply("/bash - Access denied.");
 		let exec = require('child_process').exec;
 		exec(target, (error, stdout, stderr) => {
