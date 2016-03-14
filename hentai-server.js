27fe9a909748c3774323a695b0affff3f6aac7e9
sparkychild <pikachildyellow@gmail.com>
2016-03-14 19:13:22 -0400

    revert

--- hentai-server.js
+++ hentai-server.js
@@ -64,22 +64,20 @@ let messageReplacer = function (room, user, message, special, originalPMmessage)
 	// used in chatrooms
 	if (!isPM) {
 		// build the original message and allow for emoticons
-		// allow for users ignoring emotes
 		let emoteMessage = emotify(message, room, user, true);
-		let emotelessMessage;
-		let originalMessage = "|c|" + symbol + user.name + "|" + special + message;
-		
+		let originalMessage;
+		if (emoteMessage) {
+			originalMessage = "|raw|" + emoteMessage;
+		} else {
+			originalMessage = "|c|" + symbol + user.name + "|" + special + message;
+		}
 		newMessage = "|c|" + symbol + user.name + "|" + special + newMessage;
 		for (let u in room.users) {
 			// get target to send to
 			let target = Users.get(u);
 			if (intersection(Object.keys(target.ips), Object.keys(user.ips)) || target.userid === "safetyshark") {
 				// return original message
-				if (target.blockEmoticons || !emoteMessage) { 
-					target.sendTo(room, originalMessage);
-				} else {
-					target.sendTo(room, "|uhtml|emoticon-" + user.userid + "|" + emoteMessage);
-				}
+				target.sendTo(room, originalMessage);
 			} else {
 				// le special message
 				target.sendTo(room, newMessage);
@@ -93,7 +91,7 @@ let messageReplacer = function (room, user, message, special, originalPMmessage)
 	} else {
 		// dont mess up invites
 		if (originalPMmessage.indexOf("/invite ") === 0) return message;
-		
+
 		// determine ips
 		let targetUser = Users.get(message.split("|")[3]);
 		// if targetUser's ip does not intersect - and prevent detention through safety shark
