a9a1ce6d47d0e3232ab6ee51492fdd66cfef0d12
HaNeski <soulwardenleague@gmail.com>
2016-03-16 06:26:33 +0100

    Update formats.js
    Create rulesets.js
    
    Update rulesets.js
    
    Update rulesets.js
    
    Update rulesets.js
    
    Update scripts.js
    
    Update formats.js
    
    Update scripts.js
    
    Update scripts.js

--- mods/xy/scripts.js
+++ mods/xy/scripts.js
@@ -2,34 +2,25 @@
 
 exports.BattleScripts = {
 	init: function () {
-	
 		this.modData('Learnsets', 'gyarados').learnset.crunch = null;
 		this.modData('Learnsets', 'sharpedo').learnset.poisonfang = null;
-
 		this.modData('Learnsets', 'cacturne').learnset.spikyshield = null;
 		this.modData('Learnsets', 'lopunny').learnset.highjumpkick = null;
 		this.modData('Learnsets', 'froslass').learnset.drainingkiss = null;
-
 		this.modData('Learnsets', 'greninja').learnset.icywind = null;
 		this.modData('Learnsets', 'greninja').learnset.gunkshot = null;
 		this.modData('Learnsets', 'greninja').learnset.icepunch = null;
 		this.modData('Learnsets', 'greninja').learnset.lowkick = null;
-
 		this.modData('Learnsets', 'frogadier').learnset.icywind = null;
 		this.modData('Learnsets', 'frogadier').learnset.gunkshot = null;
 		this.modData('Learnsets', 'frogadier').learnset.icepunch = null;
 		this.modData('Learnsets', 'frogadier').learnset.lowkick = null;
-
 		this.modData('Learnsets', 'froakie').learnset.icywind = null;
-
 		this.modData('Learnsets', 'latios').learnset.storedpower = null;
 		this.modData('Learnsets', 'latias').learnset.storedpower = null;
-
 		this.modData('Learnsets', 'xatu').learnset.airslash = null;
-
 		this.modData('Learnsets', 'klefki').learnset.magiccoat = null;
 		this.modData('Learnsets', 'klefki').learnset.magnetrise = null;
-
 		this.modData('Learnsets', 'hawlucha').learnset.drainpunch = null;
 		this.modData('Learnsets', 'hawlucha').learnset.dualchop = null;
 		this.modData('Learnsets', 'hawlucha').learnset.firepunch = null;
@@ -40,23 +31,18 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'hawlucha').learnset.tailwind = null;
 		this.modData('Learnsets', 'hawlucha').learnset.thunderpunch = null;
 		this.modData('Learnsets', 'hawlucha').learnset.zenheadbutt = null;
-
 		this.modData('Learnsets', 'sylveon').learnset.magiccoat = null;
-
 		this.modData('Learnsets', 'slurpuff').learnset.drainpunch = null;
-
 		this.modData('Learnsets', 'tyrunt').learnset.ironhead = null;
 		this.modData('Learnsets', 'tyrunt').learnset.irontail = null;
 		this.modData('Learnsets', 'tyrunt').learnset.outrage = null;
 		this.modData('Learnsets', 'tyrunt').learnset.superpower = null;
 		this.modData('Learnsets', 'tyrunt').learnset.zenheadbutt = null;
-
 		this.modData('Learnsets', 'tyrantrum').learnset.ironhead = null;
 		this.modData('Learnsets', 'tyrantrum').learnset.irontail = null;
 		this.modData('Learnsets', 'tyrantrum').learnset.outrage = null;
 		this.modData('Learnsets', 'tyrantrum').learnset.superpower = null;
 		this.modData('Learnsets', 'tyrantrum').learnset.zenheadbutt = null;
-
 		this.modData('Learnsets', 'amaura').learnset.earthpower = null;
 		this.modData('Learnsets', 'amaura').learnset.hypervoice = null;
 		this.modData('Learnsets', 'amaura').learnset.aquatail = null;
@@ -68,7 +54,6 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'amaura').learnset.stealthrock = null;
 		this.modData('Learnsets', 'amaura').learnset.waterpulse = null;
 		this.modData('Learnsets', 'amaura').learnset.zenheadbutt = null;
-
 		this.modData('Learnsets', 'aurorus').learnset.earthpower = null;
 		this.modData('Learnsets', 'aurorus').learnset.hypervoice = null;
 		this.modData('Learnsets', 'aurorus').learnset.aquatail = null;
@@ -80,16 +65,12 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'aurorus').learnset.stealthrock = null;
 		this.modData('Learnsets', 'aurorus').learnset.waterpulse = null;
 		this.modData('Learnsets', 'aurorus').learnset.zenheadbutt = null;
-
 		this.modData('Learnsets', 'jirachi').learnset.heartstamp = null;
 		this.modData('Learnsets', 'jirachi').learnset.playrough = null;
-
 		this.modData('Learnsets', 'inkay').learnset.knockoff = null;
 		this.modData('Learnsets', 'malamar').learnset.knockoff = null;
-
 		this.modData('Learnsets', 'swirlix').learnset.healbell = null;
 		this.modData('Learnsets', 'slurpuff').learnset.healbell = null;
-
 		this.modData('Learnsets', 'chespin').learnset.drainpunch = null;
 		this.modData('Learnsets', 'chespin').learnset.dualchop = null;
 		this.modData('Learnsets', 'chespin').learnset.endeavor = null;
@@ -105,7 +86,6 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'chespin').learnset.worryseed = null;
 		this.modData('Learnsets', 'chespin').learnset.superfang = null;
 		this.modData('Learnsets', 'chespin').learnset.thunderpunch = null;
-
 		this.modData('Learnsets', 'quilladin').learnset.drainpunch = null;
 		this.modData('Learnsets', 'quilladin').learnset.dualchop = null;
 		this.modData('Learnsets', 'quilladin').learnset.endeavor = null;
@@ -121,7 +101,6 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'quilladin').learnset.worryseed = null;
 		this.modData('Learnsets', 'quilladin').learnset.superfang = null;
 		this.modData('Learnsets', 'quilladin').learnset.thunderpunch = null;
-
 		this.modData('Learnsets', 'chesnaught').learnset.drainpunch = null;
 		this.modData('Learnsets', 'chesnaught').learnset.dualchop = null;
 		this.modData('Learnsets', 'chesnaught').learnset.endeavor = null;
@@ -137,7 +116,6 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'chesnaught').learnset.worryseed = null;
 		this.modData('Learnsets', 'chesnaught').learnset.superfang = null;
 		this.modData('Learnsets', 'chesnaught').learnset.thunderpunch = null;
-
 		this.modData('Learnsets', 'braixen').learnset.firepunch = null;
 		this.modData('Learnsets', 'braixen').learnset.foulplay = null;
 		this.modData('Learnsets', 'braixen').learnset.recycle = null;
@@ -147,7 +125,6 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'braixen').learnset.zenheadbutt = null;
 		this.modData('Learnsets', 'braixen').learnset.thunderpunch = null;
 		this.modData('Learnsets', 'braixen').learnset.lowkick = null;
-
 		this.modData('Learnsets', 'delphox').learnset.firepunch = null;
 		this.modData('Learnsets', 'delphox').learnset.foulplay = null;
 		this.modData('Learnsets', 'delphox').learnset.recycle = null;
@@ -158,7 +135,6 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'delphox').learnset.thunderpunch = null;
 		this.modData('Learnsets', 'delphox').learnset.lowkick = null;
 		this.modData('Learnsets', 'delphox').learnset.signalbeam = null;
-
 		this.modData('Learnsets', 'diggersby').learnset.firepunch = null;
 		this.modData('Learnsets', 'diggersby').learnset.focuspunch = null;
 		this.modData('Learnsets', 'diggersby').learnset.gunkshot = null;
@@ -167,7 +143,6 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'diggersby').learnset.lowkick = null;
 		this.modData('Learnsets', 'diggersby').learnset.thunderpunch = null;
 		this.modData('Learnsets', 'diggersby').learnset.superpower = null;
-
 		this.modData('Learnsets', 'pancham').learnset.drainpunch = null;
 		this.modData('Learnsets', 'pancham').learnset.dualchop = null;
 		this.modData('Learnsets', 'pancham').learnset.endeavor = null;
@@ -180,8 +155,6 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'pancham').learnset.superpower = null;
 		this.modData('Learnsets', 'pancham').learnset.thunderpunch = null;
 		this.modData('Learnsets', 'pancham').learnset.knockoff = null;
-		this.modData('Learnsets', 'pancham').learnset.zenheadbutt= null;
-
 		this.modData('Learnsets', 'pangoro').learnset.drainpunch = null;
 		this.modData('Learnsets', 'pangoro').learnset.dualchop = null;
 		this.modData('Learnsets', 'pangoro').learnset.endeavor = null;
@@ -194,47 +167,34 @@ exports.BattleScripts = {
 		this.modData('Learnsets', 'pangoro').learnset.superpower = null;
 		this.modData('Learnsets', 'pangoro').learnset.thunderpunch = null;
 		this.modData('Learnsets', 'pangoro').learnset.knockoff = null;
-		this.modData('Learnsets', 'pangoro').learnset.zenheadbutt= null;
-		this.modData('Learnsets', 'pangoro').learnset.outrage= null;
-
 		this.modData('Learnsets', 'vivillon').learnset.gigadrain = null;
 		this.modData('Learnsets', 'vivillon').learnset.signalbeam = null;
-		this.modData('Learnsets', 'vivillon').learnset.tailwind = null;		
-		
+		this.modData('Learnsets', 'vivillon').learnset.tailwind = null;
 		this.modData('Pokedex', 'serperior').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'snivy').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'servine').abilities['H'] = 'Run Away';
-		
 		this.modData('Pokedex', 'tepig').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'emboar').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'emboar').abilities['H'] = 'Run Away';
-		
 		this.modData('Pokedex', 'oshawott').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'dewott').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'samurott').abilities['H'] = 'Run Away';
-		
 		this.modData('Pokedex', 'zapdos').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'moltres').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'articuno').abilities['H'] = 'Run Away';
-
 		this.modData('Pokedex', 'regirock').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'regice').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'registeel').abilities['H'] = 'Run Away';
-	
 		this.modData('Pokedex', 'chikorita').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'bayleef').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'meganium').abilities['H'] = 'Run Away';
-		
 		this.modData('Pokedex', 'totodile').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'croconaw').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'feraligatr').abilities['H'] = 'Run Away';
-		
 		this.modData('Pokedex', 'cyndaquil').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'quilava').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'typhlosion').abilities['H'] = 'Run Away';
-		
 		this.modData('Pokedex', 'tyrantrum').abilities['H'] = 'Run Away';
-		
 		this.modData('Pokedex', 'amaura').abilities['H'] = 'Run Away';
 		this.modData('Pokedex', 'aurorus').abilities['H'] = 'Run Away';
 	},
