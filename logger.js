855ec93991d2089dd2b0cc907e3aa818cfd9c27f
Phil <birkal@outlook.com>
2016-03-20 13:34:46 -0700

    Remove backup save

--- logger.js
+++ logger.js
@@ -2,10 +2,6 @@
 
 const request = require('request');
 const token = 'superSecretToken349859489DONOTSHAREPLZ294';
-const SIX_HOURS = 21600000;
-
-let tempData = [];
-let lastSentLog = Date.now();
 
 function logMessage(data) {
 	request.post('http://origin-data.herokuapp.com/add', {
@@ -19,21 +15,6 @@ function logMessage(data) {
 		},
 	}, (err, response, body) => {
 		if (err) console.error(err);
-		// save data to temporary array when error or data server is down
-		if (body !== '"success"') {
-			tempData.push(data);
-			if ((Date.now() - lastSentLog) > SIX_HOURS) {
-				let tmp = tempData;
-				lastSentLog = Date.now();
-				tempData = [];
-				for (let i = 0; i < tmp.length; i++) {
-					logMessage(tmp[i]);
-				}
-			}
-		} else {
-			lastSentLog = Date.now();
-			tempData = [];
-		}
 	});
 }
 
