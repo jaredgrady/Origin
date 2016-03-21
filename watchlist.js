7d559c15406182e4d64516fbfb00c4603748bd1e
sparkychild <pikachildyellow@gmail.com>
2016-03-21 03:44:01 -0400

    Update watchlist.js

--- watchlist.js
+++ watchlist.js
@@ -3,10 +3,11 @@ const targetRoom = Rooms.get(roomId);
 let targetUsers = ["paulcentury"];
 
 const onMessage = function (user, room, message) {
-  if (targetRoom && targetUsers.indexOf(user.userid) > -1) targetRoom.add("|c|" + user.userid + "|__(to " + room.id + ")__ " + message);
+	if (targetRoom && targetUsers.indexOf(user.userid) > -1) targetRoom.add("|c|" + user.userid + "|__(to " + room.id + ")__ " + message);
 };
 
 module.exports = {
-  onMessage: onMessage,
-  targetUsers: targetUsers,
+	onMessage: onMessage,
+	targetUsers: targetUsers,
 };
+	
