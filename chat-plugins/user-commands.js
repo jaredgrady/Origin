a390d9d581013ef3303f8b84b05f47cc7caddbce
AuraStormLucario <yolo2infinity@icloud.com>
2016-03-31 22:59:29 -0400

    Add prank /auth

--- chat-plugins/user-commands.js
+++ chat-plugins/user-commands.js
@@ -75,6 +75,7 @@ let roomIntro = function (i) {
 };
 
 exports.commands = {
+/*
 	stafflist: 'authority',
 	auth: 'authority',
 	authlist: 'authority',
@@ -101,7 +102,7 @@ exports.commands = {
 		if (!buffer.length) buffer = "This server has no auth.";
 		connection.popup(buffer.join("\n\n"));
 	},
-
+*/
 	//Lots of these
 	afk: 'away',
 	busy: 'away',
