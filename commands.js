42aba4d000f8e46e9735a5a077dca46c963ebd9a
Irraquated <hookerjackson22@gmail.com>
2016-04-06 19:21:02 +1000

    Disallow users to ban staff members

--- commands.js
+++ commands.js
@@ -1048,7 +1048,8 @@ let commands = exports.commands = {
 			return this.errorReply("Room bans are not meant to be used in room " + room.id + ".");
 		}
 		if (targetUser.isStaff && !room.isPrivate && !room.battle && !room.isPersonal) {
-			Monitor.log("[RoomMonitor] Staff member: " + targetUser.name + " was roombanned in " + room.id + ".");
+			this.errorReply("You are not allowed to ban global staff. If you continuosly try to do so, you will be locked.");
+			return Monitor.log("[RoomMonitor] " + user.name + " tried to ban staff member: " + targetUser.name + " from the room: " + room.id + ".");
 		}
 		if (room.bannedUsers[userid] && room.bannedIps[targetUser.latestIp]) return this.sendReply("User " + targetUser.name + " is already banned from room " + room.id + ".");
 		if (targetUser in room.users || user.can('lock')) {
