99486ab98922433ec4004214b02bf7e907892f81
MasterFloat <themasterfloat@gmail.com>
2016-04-09 12:37:54 +0200

    Update user-commands.js

--- chat-plugins/user-commands.js
+++ chat-plugins/user-commands.js
@@ -589,13 +589,13 @@ exports.commands = {
 						let css = 'text-shadow: 1px 1px 1px #CCC; padding: 3px 8px;';
 						let output = '<div class="infobox"><table><tr>';
 						output += '<td style="' + css + ' background: rgba(170, 165, 215, 0.5); box-shadow: 2px 2px 5px rgba(170, 165, 215, 0.8); border: 1px solid rgba(170, 165, 215, 1); border-radius: 5px; color: #2D2B40; text-align: center; font-size: 15pt;"><b>' + data.title_romaji + '</b></td>';
-						output += '<td rowspan="7"><img src="' + data.image_url_lge + '" height="320" width="225" alt="' + data.title_romaji + '" title="' + data.title_romaji + '" style="float: right; border-radius: 10px; box-shadow: 4px 4px 3px rgba(0, 0, 0, 0.5), 1px 1px 2px rgba(255, 255, 255, 0.5) inset;" /></td></tr>';
+						output += '<td rowspan="6"><img src="' + data.image_url_lge + '" height="320" width="225" alt="' + data.title_romaji + '" title="' + data.title_romaji + '" style="float: right; border-radius: 10px; box-shadow: 4px 4px 3px rgba(0, 0, 0, 0.5), 1px 1px 2px rgba(255, 255, 255, 0.5) inset;" /></td></tr>';
 						output += '<tr><td style="' + css + '"><b>Genre(s): </b>' + data.genres + '</td></tr>';
 						output += '<tr><td style="' + css + '"><b>Air Date: </b>' + data.start_date.substr(0, 10) + '</td></tr><tr>';
 						output += '<tr><td style="' + css + '"><b>Status: </b>' + data.airing_status + '</td></tr>';
 						output += '<tr><td style="' + css + '"><b>Episode Count: </b>' + data.total_episodes + '</td></tr>';
 						output += '<tr><td style="' + css + '"><b>Rating: </b> ' + data.average_score + '/100</td></tr>';
-						output += '<tr><td style="' + css + '"><b>Description: </b>' + Tools.escapeHTML(data.description.replace(/(\r\n|\n|\r)/gm, " ")) + '</td></tr>';
+						output += '<tr><td colspan="2" style="' + css + '"><b>Description: </b>' + Tools.escapeHTML(data.description.replace(/(\r\n|\n|\r)/gm, " ")) + '</td></tr>';
 						output += '</table></div>';
 						if (output.indexOf('&lt;br&gt;&lt;br&gt;') >= 0) output = output.substr(0, output.indexOf('&lt;br&gt;&lt;br&gt;'));
 						if (output.indexOf('<br><br>') >= 0) output = output.substr(0, output.indexOf('<br><br>'));
