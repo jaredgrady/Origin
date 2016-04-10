266cecd3e58a4ebaec91b8b6f21065b82fbeea79
supersoniko <sacha.accounts@outlook.com>
2016-04-10 14:02:29 +0200

    Manga command added

--- chat-plugins/user-commands.js
+++ chat-plugins/user-commands.js
@@ -554,4 +554,43 @@ exports.commands = {
 				return this.errorReply("Anime not found.");
 			});
 	},
+	manga: function (target, room, user) {
+		if (!this.runBroadcast()) return;
+		if (room.id !== 'otakulair') return this.errorReply("This command can only be used in the Otaku Lair.");
+		if (!target) return this.errorReply("No target.");
+		let targetAnime = Tools.escapeHTML(target.trim());
+		nani.get('manga/search/' + targetAnime)
+			.then(data => {
+				nani.get('manga/' + data[0].id)
+					.then(data => {
+						let css = 'text-shadow: 1px 1px 1px #CCC; padding: 3px 8px;';
+						let output = '<div class="infobox"><table><tr>';
+						for (let i = 0; i < data.genres.length; i++) {
+							if (/(Hentai|Yaoi|Ecchi)/.test(data.genres[i])) return this.errorReply('Nsfw content is not allowed.');
+						}
+						let description = Tools.escapeHTML(data.description.replace(/(\r\n|\n|\r)/gm, " "));
+						if (description.indexOf('&lt;br&gt;&lt;br&gt;') >= 0) description = description.substr(0, description.indexOf('&lt;br&gt;&lt;br&gt;'));
+						if (description.indexOf('<br>') >= 0) description = description.substr(0, description.indexOf('<br>'));
+						output += '<td style="' + css + ' background: rgba(170, 165, 215, 0.5); box-shadow: 2px 2px 5px rgba(170, 165, 215, 0.8); border: 1px solid rgba(170, 165, 215, 1); border-radius: 5px; color: #2D2B40; text-align: center; font-size: 15pt;"><b>' + data.title_romaji + '</b></td>';
+						output += '<td rowspan="6"><img src="' + data.image_url_lge + '" height="320" width="225" alt="' + data.title_romaji + '" title="' + data.title_romaji + '" style="float: right; border-radius: 10px; box-shadow: 4px 4px 3px rgba(0, 0, 0, 0.5), 1px 1px 2px rgba(255, 255, 255, 0.5) inset;" /></td></tr>';
+						output += '<tr><td style="' + css + '"><b>Genre(s): </b>' + data.genres + '</td></tr>';
+						output += '<tr><td style="' + css + '"><b>Release Date: </b>' + data.start_date.substr(0, 10) + '</td></tr><tr>';
+						output += '<tr><td style="' + css + '"><b>Status: </b>' + data.publishing_status + '</td></tr>';
+						output += '<tr><td style="' + css + '"><b>Chapter Count: </b>' + data.total_chapters + '</td></tr>';
+						output += '<tr><td style="' + css + '"><b>Rating: </b> ' + data.average_score + '/100</td></tr>';
+						output += '<tr><td colspan="2" style="' + css + '"><b>Description: </b>' + description + '</td></tr>';
+						output += '</table></div>';
+						this.sendReply('|raw|' + output);
+						Rooms('otakulair').update();
+					})
+					.catch(error => {
+						console.log(error);
+						return this.errorReply("Anime not found.");
+					});
+			})
+			.catch(error => {
+				console.log(error);
+				return this.errorReply("Anime not found.");
+			});
+	},
 };
