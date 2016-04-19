c2b8844a7f71c4633a6919b3bbe416db2b1e2e25
thefenderstory <jgrady15@brophybroncos.org>
2016-04-19 11:19:48 -0700

    Create gym-roster.js

--- chat-plugins/gym-roster.js
+++ chat-plugins/gym-roster.js
@@ -26,24 +26,24 @@ const typeImg = {
 
 class Gymleaders {
 	constructor() {
-		this.normal = 'open';
-		this.fire = 'open';
-		this.water = 'open';
-		this.grass = 'open';
-		this.electric = 'open';
-		this.fighting = 'open';
-		this.flying = 'open';
-		this.poison = 'open';
-		this.ground = 'open';
-		this.rock = 'open';
-		this.bug = 'open';
-		this.psychic = 'open';
-		this.ghost = 'open';
-		this.steel = 'open';
-		this.dark = 'open';
-		this.ice = 'open';
-		this.dragon = 'open';
-		this.fairy = 'open';
+		this.normal = ['open', 'open'];
+		this.fire = ['open', 'open'];
+		this.water = ['open', 'open'];
+		this.grass = ['open', 'open'];
+		this.electric = ['open', 'open'];
+		this.fighting = ['open', 'open'];
+		this.flying = ['open', 'open'];
+		this.poison = ['open', 'open'];
+		this.ground = ['open', 'open'];
+		this.rock = ['open', 'open'];
+		this.bug = ['open', 'open'];
+		this.psychic = ['open', 'open'];
+		this.ghost = ['open', 'open'];
+		this.steel = ['open', 'open'];
+		this.dark = ['open', 'open'];
+		this.ice = ['open', 'open'];
+		this.dragon = ['open', 'open'];
+		this.fairy = ['open', 'open'];
 	}
 }
 
@@ -61,15 +61,19 @@ function typeTag(link, name) {
 
 function gymDisplay(room) {
 	let data = Object.keys(room.gymleaders);
-	let output = '<table> <tr>';
+	let output = '<center><h4><u>' + room.title + ' Gym Leaders</u></h4></center><table><tr>';
+	let double = '';
+	let onlineA = "#e71414";
+	let onlineB = "#e71414";
 	for (let i = 0; i < data.length; i++) {
-		output += '<td>' + typeTag(typeImg[data[i]], data[i]) + '</td> <td>' + room.gymleaders[data[i]] + '</td>';
-		if (!Users(room.gymleaders[data[i]])) {
-			output += '<td><font color="#e71414">Offline</font></td>';
-		} else {
-			output += '<td><font color="#2ECC40">Online</font></td>';
-		}
+		if (Users(room.gymleaders[data[i]][0])) onlineA = "#2ECC40";
+		if (Users(room.gymleaders[data[i]][1])) onlineB = "#2ECC40";
+		if (room.gymleaders[data[i]][1] !== 'open') double = " - " + font(onlineB, room.gymleaders[data[i]][1]);
+		output += '<td>' + typeTag(typeImg[data[i]], data[i]) + '</td> <td>' + font(onlineA, room.gymleaders[data[i]][0]) + double + '</td>';
 		if (i % 2 === 1) output += '</tr> <tr>';
+		double = '';
+		onlineA = "#e71414";
+		onlineB = "#e71414";
 	}
 	output += '</tr> <table>';
 	return output;
@@ -89,41 +93,50 @@ exports.commands = {
 			if (!this.can('declare', null, room)) return false;
 			if (!room.chatRoomData.gymleaders) room.gymleaders = room.chatRoomData.gymleaders = new Gymleaders();
 			let parts = target.split(',');
+			let position = 0;
 			if (parts.length < 2) return this.errorReply("You must specify a type and user.");
 			let type = toId(parts[0]);
 			let gymLeader = toId(parts[1]);
 			if (!room.gymleaders[type]) return this.errorReply("Type: " + type + " not found. Did you spell it correctly?");
-			if (room.gymleaders[type] !== 'open') return this.errorReply("This gym leader position is already filled. Use /gym replace to remove them and add a new leader.");
-			room.gymleaders[type] = gymLeader;
-			room.chatRoomData.gymleaders[type] = gymLeader;
+			if (room.gymleaders[type][0] !== 'open' && room.gymleaders[type][1] !== 'open') return this.errorReply("These gym leader positions are already filled. Use /gym replace to remove them and add a new leader.");
+			if (room.gymleaders[type][0] !== 'open') position = 1;
+			room.gymleaders[type][position] = gymLeader;
+			room.chatRoomData.gymleaders[type][position] = gymLeader;
 			Rooms.global.writeChatRoomData();
 			room.add(gymLeader + " has been appointed as the " + type + " gym leader for this league by " + user + ".");
 		},
 		remove: function (target, room, user) {
 			if (!this.can('declare', null, room)) return false;
-			let type = toId(target);
+			let parts = target.split(',');
+			let type = toId(parts[0]);
+			let position = 0;
+			if (parts.length < 2) return this.errorReply("You must specify a type and user.");
+			let gymLeader = toId(parts[1]);
 			if (!room.gymleaders) return this.errorReply("This room has no gym leaders set.");
 			if (!room.gymleaders[type]) return this.errorReply("Type: " + type + " not found. Did you spell it correctly?");
-			if (room.gymleaders[type] === 'open') return this.errorReply("This gym leader position is already open.");
-			room.gymleaders[type] = 'open';
-			room.chatRoomData.gymleaders[type] = 'open';
+			if (room.gymleaders[type][0] === 'open' && room.gymleaders[type][1] === 'open') return this.errorReply("All gym leader positions for this type are already open.");
+			if (room.gymleaders[type][0] !== gymLeader) position = 1;
+			room.gymleaders[type][position] = 'open';
+			room.chatRoomData.gymleaders[type][position] = 'open';
 			Rooms.global.writeChatRoomData();
-			this.sendReply(target + " has been removed from the position of" + type + " gym leader.");
-			this.addModCommand(target + " has been removed from the position of" + type + " gym leader by " + user + ".");
+			this.addModCommand("(" + gymLeader + " has been removed from the position of" + type + " gym leader by " + user + ".)");
 		},
 		replace: function (target, room, user) {
 			if (!this.can('declare', null, room)) return false;
 			if (!room.gymleaders) return this.errorReply("This room has no gym leaders set.");
 			let parts = target.split(',');
-			if (parts.length < 2) return this.errorReply("You must specify a type and user.");
+			if (parts.length < 3) return this.errorReply("You must specify a type and user.");
 			let type = toId(parts[0]);
 			let gymLeader = toId(parts[1]);
+			let newLeader = toId(parts[2]);
+			let position = 0;
 			if (!room.gymleaders[type]) return this.errorReply("Type: " + type + " not found. Did you spell it correctly?");
-			if (room.gymleaders[type] === 'open') return this.errorReply("This gym leader position is currently open. Use /gym add to set a new leader.");
-			room.gymleaders[type] = gymLeader;
-			room.chatRoomData.gymleaders[type] = gymLeader;
+			if (room.gymleaders[type][0] === 'open' && room.gymleaders[type][1] === 'open') return this.errorReply("All gym leader positions for this type are already open.");
+			if (room.gymleaders[type][0] !== gymLeader) position = 1;
+			room.gymleaders[type][position] = newLeader;
+			room.chatRoomData.gymleaders[type][position] = newLeader;
 			Rooms.global.writeChatRoomData();
-			room.add(target + " has been appointed as the " + type + " gym leader for this league by " + user + ".");
+			room.add(newLeader + " has been appointed as the " + type + " gym leader for this league by " + user + ".");
 		},
 	},
 };
