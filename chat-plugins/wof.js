3a3415fb5de36322b2b8a3ac855094e8d3546ecc
Jared Grady <jared@thekards.com>
2016-04-22 15:45:05 -0700

    Fix travis error

--- chat-plugins/wof.js
+++ chat-plugins/wof.js
@@ -85,8 +85,8 @@ exports.commands = {
 			if (room.id !== 'casino') return this.errorReply('Wheel of Fortune can only be played in the "Casino".');
 			if (!this.canTalk()) return this.errorReply('/wheel spin - Access Denied.');
 			if (!enable) return this.errorReply('Wheel of fortune is temporarily disabled.');
+			let amount = Db('money').get(user.userid, 0);
 			Db('money').set(user.userid, (amount - 3));
-			const amount = Db('money').get(user.userid, 0);
 			if (amount < 3) return this.errorReply('You don\'t have enough bucks to play this game. You need ' + (3 - amount) + currencyName(amount) + ' more.');
 
 			const result = spin();
