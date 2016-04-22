b5bbe27ca0d864f42af8048ef00bf0e4c112d9b5
Wando94 <daniel.wilson11@outlook.com>
2016-04-22 23:40:11 +0100

    Update wof.js

--- chat-plugins/wof.js
+++ chat-plugins/wof.js
@@ -85,6 +85,7 @@ exports.commands = {
 			if (room.id !== 'casino') return this.errorReply('Wheel of Fortune can only be played in the "Casino".');
 			if (!this.canTalk()) return this.errorReply('/wheel spin - Access Denied.');
 			if (!enable) return this.errorReply('Wheel of fortune is temporarily disabled.');
+			Db('money').set(user.userid, (amount - 3));
 			const amount = Db('money').get(user.userid, 0);
 			if (amount < 3) return this.errorReply('You don\'t have enough bucks to play this game. You need ' + (3 - amount) + currencyName(amount) + ' more.');
 
@@ -96,7 +97,6 @@ exports.commands = {
 				Db('money').set(user.userid, (amount + spins[result]));
 				return this.sendReplyBox(display(true, user.name, result));
 			}
-			Db('money').set(user.userid, (amount - 3));
 			return this.sendReplyBox(display(false, user.name, spin()));
 		},
 		'toggle': function (target, room, user) {
