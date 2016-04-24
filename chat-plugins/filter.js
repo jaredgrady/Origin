983e9d9f12003d68c91525d1a3905b28b7b8ca1f
AuraStormLucario <yolo2infinity@icloud.com>
2016-04-24 00:30:44 -0400

    Update filter.js

--- chat-plugins/filter.js
+++ chat-plugins/filter.js
@@ -20,15 +20,15 @@ exports.filterMessage = function (message) {
 };
 
 exports.commands = {
-	filter: "chatfilter",
+	filter: 'chatfilter',
 	chatfilter: function (target, room, user, connection, cmd) {
-		if (!user.can("declare")) return this.errorReply("The command '/" + cmd + "' was unrecognized. To send a message starting with '/" + cmd + "', type '//" + cmd + "'.");
+		if (!user.can('declare')) return this.errorReply("The command '/" + cmd + "' was unrecognized. To send a message starting with '/" + cmd + "', type '//" + cmd + "'.");
 		if (!target) return this.errorReply("/chatfilter [add | remove | regex | list](, [phrase / regex])");
 		let parts = target.split(",");
 		let action = toId(parts[0]);
 		let selection = parts.slice(1).join(",").trim();
 		switch (action) {
-		case "add":
+		case 'add':
 			if (!selection) return this.errorReply("/chatfilter [add | remove | regex | list](, [phrase / regex])");
 			// de-regexify this.
 			selection = selection.replace(/([^a-z0-9\s])/g, m => "\\" + m);
@@ -37,7 +37,7 @@ exports.commands = {
 			buildFilterRegex();
 			this.parse('/chatfilter list');
 			break;
-		case "remove":
+		case 'remove':
 			if (!selection) return this.errorReply("/chatfilter [add | remove | regex | list](, [phrase / regex])");
 			if (!Db('filter').get(selection, null)) {
 				// check if there is a regexified version
@@ -48,8 +48,8 @@ exports.commands = {
 			buildFilterRegex();
 			this.parse('/chatfilter list');
 			break;
-		case "regex":
-			if (!this.can("bypassall")) return false;
+		case 'regex':
+			if (!this.can('bypassall')) return false;
 			if (!selection) return this.errorReply("/chatfilter [add | remove | regex | list](, [phrase / regex])");
 			if (/(?!\\)\(.*?(?:[^\\])[\*\+\?][^\)]*?(?!\\)\)([\*\+]|\{[0-9]+(\,|\,?[0-9]*?)\})/i.test(selection)) return this.errorReply("Your regular expression may contain some evil regex.  If you still wish to add this, use /chatfilter forceregex, " + selection);
 			try {
@@ -62,8 +62,8 @@ exports.commands = {
 			this.parse('/chatfilter list');
 			break;
 		// forceregex is for bypassing patterns that is otherwise limited by the ReDoS checker.
-		case "forceregex":
-			if (!this.can("bypassall")) return false;
+		case 'forceregex':
+			if (!this.can('bypassall')) return false;
 			if (!selection) return this.errorReply("/chatfilter [add | remove | regex | list](, [phrase / regex])");
 			if (action === "forceregex" && ["sparkychild", "fender"].indexOf(user.userid) === -1) return this.errorReply("You do not have premission to enter a potentially dangerous regular expression. Use /chatfilter regex, " + selection + " instead.");
 			try {
